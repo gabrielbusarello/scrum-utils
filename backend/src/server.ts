25455c705d452a4d8251ecdeb57896740e03edb4
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2021-06-21 23:42:23 -0300

    Does the websocket works, without redis

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,68 +15,77 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-server.on()
+const roomsArray: Rooms = [];
 
-wss.on('connection', (ws: WebSocket, req: http.IncomingMessage, client: WebSocket) => {
+wss.on('connection', async (ws: WebSocket, req: http.IncomingMessage) => {
     let room = (req.headers.room as string | undefined);
     const username = (req.headers.username as string | undefined);
 
-    ws.on('open', async (ws: WebSocket) => {
-    
-        if (room && username) {
-            if (room === 'new') {
-                room = new Date().getTime().toString();
-            }
-    
-            const rooms = await redis.get('rooms');
-            
-            if (rooms) {
-                const roomsJ: Rooms = JSON.parse(rooms);
-    
-                const roomFinded = roomsJ.find((roomJ) => roomJ.room === room);
-    
-                if (roomFinded) {
-                    if (!roomFinded.users.find((user) => user.username === username)) {
-                        roomFinded.users.push({ username, ws: client });
-                    }
-    
-                    //connection is up, let's add a simple simple event
-                } else {
-                    roomsJ.push({ room, users: [{ username, ws: client }] });
-                }
-                redis.set('rooms', JSON.stringify(roomsJ));
-            } else {
-                redis.set('rooms', '[]');
-            }
-        
-            // const value = await redis.get('teste');
-            //send immediatly a feedback to the incoming connection    
-            ws.send('Hi there, I am a WebSocket server. ROOM ->' + room);
-        } else {
-            ws.send('Room not informed.');
-            ws.close();
+    if (room && username) {
+        if (room === 'new') {
+            room = new Date().getTime().toString();
         }
-    })
+
+        // const rooms = await redis.get('rooms');
+        const rooms = roomsArray;
+
+        if (rooms) {
+
+            const roomFinded = rooms.find((roomJ) => roomJ.room === room);
+
+            if (roomFinded) {
+                if (!roomFinded.users.find((user) => user.username === username)) {
+                    roomFinded.users.push({ username, ws });
+                }
+
+                //connection is up, let's add a simple simple event
+            } else {
+                rooms.push({ room, users: [{ username, ws }] });
+            }
+            // redis.set('rooms', JSON.stringify(roomsJ));
+            // const roomsJ: Rooms = JSON.parse(rooms);
+
+            // const roomFinded = roomsJ.find((roomJ) => roomJ.room === room);
+
+            // if (roomFinded) {
+            //     if (!roomFinded.users.find((user) => user.username === username)) {
+            //         roomFinded.users.push({ username, ws: client });
+            //     }
+
+            //     //connection is up, let's add a simple simple event
+            // } else {
+            //     roomsJ.push({ room, users: [{ username, ws: client }] });
+            // }
+            // redis.set('rooms', JSON.stringify(roomsJ));
+        } else {
+            redis.set('rooms', '[]');
+        }
+
+        // const value = await redis.get('teste');
+        //send immediatly a feedback to the incoming connection    
+        ws.send('Hi there, I am a WebSocket server. ROOM ->' + room);
+    } else {
+        ws.send('Room not informed.');
+        ws.close();
+    }
 
     ws.on('message', async (message: string) => {
         const rooms = await redis.get('rooms');
 
         if (rooms) {
-            const roomsJ: Rooms = JSON.parse(rooms);
+            const roomsJ: Rooms = roomsArray;
             const roomJ = roomsJ.find((roomJ) => roomJ.room === room);
-    
+
             //log the received message and send it back to the client
             console.log('received: %s', message);
-    
+
             const broadcastRegex = /^broadcast\:/;
-    
+
             if (roomJ) {
                 roomJ.users.forEach(user => {
-                    ws.send('hello guys');
-                    user.ws.send('sender guys');
-                    // if (user.ws !== ws) {
-                    //     user.ws.send(`Hello, broadcast message -> ${message}`);
-                    // }
+                    if (user.ws !== ws) {
+                        user.ws.send(`Hello, broadcast message -> ${message}`);
+                    }
                 });
             }
         }
